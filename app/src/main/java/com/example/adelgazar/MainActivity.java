b50a0a49deb745366a9e5d7f2d463d9a07f95485
Jhoswa Sam <jhoswasamanez@gmail.com>
2022-03-29 11:51:50 -0500

    animacion

--- app/src/main/java/com/example/adelgazar/MainActivity.java
+++ app/src/main/java/com/example/adelgazar/MainActivity.java
@@ -7,12 +7,15 @@ import androidx.dynamicanimation.animation.SpringAnimation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int DURACION_SPLASH = 3000; // 3 seg
+    Animation topAnim;
     LottieAnimationView logo;
 
     @Override
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         logo.setAnimation(R.raw.watermelon);
         logo.playAnimation();
 
+        topAnim = AnimationUtils.loadAnimation (this,R.anim.top_animation);
+        logo.setAnimation(topAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
